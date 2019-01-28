6c487dc4bf8947ce4f1fa5096743c2e37171adc0
vivek singh <vivek.singh@st.niituniversity.in>
2019-01-29 00:47:13 +0530

    Add files via upload

--- csvWriter.java
+++ csvWriter.java
@@ -4,6 +4,9 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.regex.Matcher; 
 import java.util.regex.Pattern;
+import java.util.Random;
+import java.io.EOFException;
+
 
 public class csvWriter
  {
@@ -22,22 +25,29 @@ public class csvWriter
   
   public static void main(String[] args) throws IOException
   {
-      String Name= "";
-      boolean stringValidator;
+      String name= "";
+      boolean stringValidator=false;
       int id;
       String email;
-      Date date = new Date();
-      List<String> City={"Delhi","chennai","Banglore", "Karnataka","Mumbai"};
-      List<String> State={"Delhi","Tamil Naidu","Karnataka","Maharashtra"};
+      //Date date = new Date();
+      String[] City={"Delhi","chennai","Banglore", "Karnataka","Mumbai"};
+      String[] State={"Delhi","Tamil Naidu","Karnataka","Maharashtra"};
       String Country="India";
 
 
-      Scanner sc = new Scanner(Sytem.in);
-      name= sc.next();
-        if(name.length>5 && name.length<12)
+      Scanner sc = new Scanner(System.in);
+      String name1= sc.next();
+        if((name1.length()>5) && (name1.length()<12))
         {
             stringValidator = true;
+            name= name1;
+            
         }
+        else
+        {
+            System.out.println("wrong username");
+        name=sc.next();
+    }
       id= sc.nextInt();
 
         email = sc.next(); 
@@ -69,6 +79,31 @@ public class csvWriter
    out.println("Country");// second row third column
       
    //Flush the output to the file
+   Random rand = new Random();
+
+
+   
+   while(true)
+   {
+        out.println();
+        if(stringValidator)
+        {
+        out.print(name);
+        }
+        out.print(",");
+        out.print(id);
+        out.print(",");
+        out.print(email);
+        out.print(",");
+        int  n = rand.nextInt(4) + 0;
+        out.print(City[n]);
+        out.print(",");
+        out.print(State[n]);
+        out.print(",");
+        out.print(Country);
+   }
+   
+   
    out.flush();
        
    //Close the Print Writer
